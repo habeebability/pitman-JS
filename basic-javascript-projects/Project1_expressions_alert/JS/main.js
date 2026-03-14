2cc0fbc9c57c4ff05fe50f1f7a85426e4a89818a
Habeebability <awoyemi.habeeb@gmail.com>
2026-03-14 16:25:31 +0000

    comments and concatenation

--- basic-javascript-projects/Project1_expressions_alert/JS/main.js
+++ basic-javascript-projects/Project1_expressions_alert/JS/main.js
@@ -2,13 +2,21 @@
 // window.alert("hello world") 
 // document.writeln("Hello world")
 
+
+// variables.
 var A = "String A"
 var B = "How are you?"
 var C = "Concatenated" + "string"
+var D = 4 + 5
 
 document.writeln(A)
 
-
+// document.writeln
 document.writeln("Hey " + B)
 
+// document.write method
 document.writeln(C)
+
+// Expression
+document.writeln(D)
+
