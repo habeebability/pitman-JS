11e026d8bf7f92225728c3db15df735be2c0cdce
Habeebability <awoyemi.habeeb@gmail.com>
2026-03-14 16:00:09 +0000

    added alert,windows, doc.write and concatenation assignments & challenge

--- basic-javascript-projects/Project1_expressions_alert/JS/main.js
+++ basic-javascript-projects/Project1_expressions_alert/JS/main.js
@@ -1,1 +1,14 @@
-alert("hello world")
+// alert("hello world")
+// window.alert("hello world") 
+// document.writeln("Hello world")
+
+var A = "String A"
+var B = "How are you?"
+var C = "Concatenated" + "string"
+
+document.writeln(A)
+
+
+document.writeln("Hey " + B)
+
+document.writeln(C)
