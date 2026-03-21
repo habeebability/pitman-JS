e0421f66a4d25b039d47dd8de249298378581ff2
Habeebability <awoyemi.habeeb@gmail.com>
2026-03-21 20:55:49 +0000

    Project3_Math_Operators Assignment

--- basic-javascript-projects/Project3_math_operators/JS/main.js
+++ basic-javascript-projects/Project3_math_operators/JS/main.js
@@ -1,3 +1,5 @@
+
+// Add
 function addNumbers(){
     let num1 = 5;
     let num2 = 12;
@@ -5,7 +7,7 @@ function addNumbers(){
     document.getElementById("Math").innerHTML = num1 + num2;
 }
 
-
+// Subsctract
 function substractNumbers(){
     let num1 = 10;
     let num2 = 3
@@ -13,31 +15,52 @@ function substractNumbers(){
     document.getElementById("substract").innerHTML = num1 - num2;
 }
 
+// Multiplication
 function multiplication(){
     let math = 2 * 8;
 
     document.getElementById("multiplication").innerHTML = math;
 }
 
+// Division
 function division(){
     let math = 8 / 2;
 
     document.getElementById("division").innerHTML = math;
 }
 
+// Operators
 function operators(){
     let math = (1 + 2) * 10 / (20 + 87)
 
     document.getElementById("operators").innerHTML = " 1 plus 2 multiplied by 10 divided twenty plus eighty seven" + math;
 }
 
+// Modulus
 function modulus(){
     let math = 10 % 4;
 
     document.getElementById("modulus").innerHTML = math;
 }
+
+// Negation
 function negation(){
     let math = 10;
 
     document.getElementById("negation").innerHTML = -math;
+}
+
+// Increment
+function increment(){
+    document.getElementById("increment").innerHTML++;
+}
+// Decrement
+function decrement(){
+    document.getElementById("decrement").innerHTML--;
+}
+
+// Random and Math Object
+function random(){
+    let math =  Math.random() * 10
+    document.getElementById("random").innerHTML = Math.round(math);
 }
